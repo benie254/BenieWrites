690281c460a55d60ec56b0a302f0d07791cdff3e
benie254 <davinci.monalissa@gmail.com>
2022-12-14 11:13:10 +0300

    update app

--- Benie/src/app/modules/home/home.module.ts
+++ Benie/src/app/modules/home/home.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
 import { NavComponent } from './nav/nav.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { StoriesComponent } from './stories/stories.component';
+import { AboutComponent } from './about/about.component';
+import { ReadComponent } from './read/read.component';
+
 
 
 
@@ -15,12 +22,18 @@ import { StoriesComponent } from './stories/stories.component';
     HomeComponent,
     NavComponent,
     StoriesComponent,
+    AboutComponent,
+    ReadComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    FormsModule,
     MatButtonModule,
     MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ]
 })
 export class HomeModule { }
