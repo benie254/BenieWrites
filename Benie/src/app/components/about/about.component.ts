f1641344f38cc9b1b57323fe5f33e9c259423957
benie254 <davinci.monalissa@gmail.com>
2023-01-08 09:26:24 +0300

    convert hard-coded data to variables

--- Benie/src/app/components/about/about.component.ts
+++ Benie/src/app/components/about/about.component.ts
@@ -12,6 +12,17 @@ import { StoryService } from 'src/app/modules/admin/services/story/story.service
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  currentSite = window.location.href;
+  author = 'Benie Langat';
+  tag = 'With words, I give my thoughts life.';
+  about = 'Benie is a passionate writer and software developer. He enjoys the challenge in chess and loves the freedom in swimming. He loves music and art.';
+  closing = 'Benie writes from the depths of the heart, for you and for his soul.';
+  img = 'https://res.cloudinary.com/benie/image/upload/v1670974359/undraw_reading_book_re_kqpk_y5d2ju.svg';
+  authorImg = 'https://res.cloudinary.com/benie/image/upload/v1671583589/IMG_20220119_175206_763_lb4sg3-removebg-preview_dkprqf.png';
+  fullName = 'Benson Langat';
+  shortName = 'Benie';
+  address = 'Nairobi, Kenya';
+  email = 'davinci.monalissa@gmail.com';
   editor: Editor;
   values = '';
   subValues = '';
